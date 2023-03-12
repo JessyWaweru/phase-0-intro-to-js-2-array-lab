f568b4133ebb4a04d65306382afaefa2b1f384fd
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-12 15:47:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveFirstCat(name){
+cats.shift()
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+}
+
+function appendCat(name){
+const appendCat=[...cats,"Broom"]
+return appendCat
+}
+
+function prependCat(name){
+const prependCat=["Arnold",...cats]
+return prependCat
+}
+
+function removeLastCat(name){
+const removeLastCat=cats.slice(0 ,cats.length - 1)
+return removeLastCat
+}
+
+function removeFirstCat(name){
+const removeFirstCat=cats.slice(1)
+return removeFirstCat
+}
+
